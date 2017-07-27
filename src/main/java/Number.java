506090add853d6ab57f23d16a9f889cfb0ba7c45
Yaxin Wang <yxWang@CNyxWang.local>
2017-07-27 16:27:04 +0800

    add factor and prime

--- src/main/java/Number.java
+++ src/main/java/Number.java
@@ -4,7 +4,25 @@ class Number {
     Number() {
     }
 
-    ArrayList<Object> primeFactors(int n) {
-        return new ArrayList<Object>();
+    ArrayList<Integer> primeFactors(int n) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0 && isPrime(i))
+                list.add(i);
+        }
+        return list;
+    }
+
+    private boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        int k = (int) Math.sqrt(n);
+        for (int i = 2; i <= k; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
--- src/test/java/PrimeFactorsTest.java
+++ src/test/java/PrimeFactorsTest.java
@@ -1,6 +1,8 @@
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 import static org.junit.Assert.assertEquals;
 
@@ -10,5 +12,20 @@ public class PrimeFactorsTest {
     @Test
     public void test_when_input_1() throws Exception {
         assertEquals(new ArrayList(), number.primeFactors(1));
+    }
+
+    @Test
+    public void test_when_input_25() throws Exception {
+        assertEquals(Collections.singletonList(5), number.primeFactors(25));
+    }
+
+    @Test
+    public void test_when_input_30() throws Exception {
+        assertEquals(Arrays.asList(2,3,5), number.primeFactors(30));
+    }
+
+    @Test
+    public void test_when_input_100() throws Exception {
+        assertEquals(Arrays.asList(2,5), number.primeFactors(100));
     }
 }
